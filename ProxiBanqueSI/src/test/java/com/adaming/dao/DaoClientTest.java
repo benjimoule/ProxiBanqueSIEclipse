34c438b023ee560ac9d44096995f17c047cac65d
AdrienJoseph <adrien.joseph22@gmail.com>
2016-04-25 10:13:02 +0200

    Mise à jour des tests JUnit

--- ProxiBanqueSI/src/test/java/com/adaming/dao/DaoClientTest.java
+++ ProxiBanqueSI/src/test/java/com/adaming/dao/DaoClientTest.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.adaming.entities.Client;
 
 import junit.framework.TestCase;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = ( "file:src/main/webapp/WEB-INF/applicationContext.xml"))
 public class DaoClientTest extends TestCase {
 
 	
